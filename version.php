b3824c8e960e8cf8360ad8d2e71b8b3800244ca9
toosa <pht854@gmail.com>
2026-01-12 18:59:04 +0700

    Naikkan versi minor ke 1.0.1 utk perbaikan company ke branch

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_hris';
-$plugin->release      = '1.0.0';
-$plugin->version      = 2025122600;
+$plugin->release      = '1.0.1';
+$plugin->version      = 2026011200;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 501];
 $plugin->maturity     = MATURITY_STABLE;
