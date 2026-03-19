7ec84a2931739f6a9a2210afc6954b47034f8149
toosa <pht854@gmail.com>
2026-03-19 12:05:54 +0700

    fix: Update version number to 1.3.6 and release date in version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_hris';
-$plugin->release      = '1.3.5';
-$plugin->version      = 2026031900;
+$plugin->release      = '1.3.6';
+$plugin->version      = 2026031901;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 501];
 $plugin->maturity     = MATURITY_STABLE;
